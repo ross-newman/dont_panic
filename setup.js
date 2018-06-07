2149123e904b88ab8171e7b3125cb228238b5729
Ross Newman <ross@rossnewman.com>
2018-06-08 00:15:09 +0100

    Adding the names (text files) and a setup script.

---
+++ setup.js
@@ -0,0 +1,54 @@
+// This is a setup script to create the setup database with the requires startup data to get going
+const readline = require('readline');
+var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var database_name = "name";
+var url = "mongodb://localhost:27017/" + database_name;
+
+console.log("Performing setup tasks (Database " + database_name + ")");
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+});
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db(database_name);
+    dbo.createCollection("customers", function (err, res) {
+        if (err) throw err;
+        console.log("Collection created!");
+        db.close();
+    });
+});
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db(database_name);
+    var names = new Array();
+
+    // Read the names in from a file
+    var myInterface = readline.createInterface({
+        input: fs.createReadStream('names.txt')
+    });
+
+    var lineno = 0;
+    myInterface.on('line', function (line) {
+        var res = line.split(" ");
+        var aname = new Object();
+        aname = JSON.parse('{"name":{"first":"' + res[0] + '" ,"last":"' + res[1] + '"}}');
+        console.log(aname.toString());
+        names[lineno] = aname;
+        lineno++;
+        console.log('Line number ' + lineno + ': ' + line);
+        dbo.collection("customers").insertOne(aname, function (err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+
+});
+
+console.log("Done...");
